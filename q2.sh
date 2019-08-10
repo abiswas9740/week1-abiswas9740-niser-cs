08d3e0b611c459b8d15b2e9c12a81579b288a7e4
Avhra Biswas <53475010+abiswas9740@users.noreply.github.com>
2019-08-10 11:45:32 +0530

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,1 +1,9 @@
-
+echo "What is your name?"
+read name
+mkdir $name
+touch $name/"name$"1.cpp
+touch $name/"name$"2.cpp
+touch $name/"name$"3.cpp
+touch $name/"name$"4.cpp
+touch $name/"name$"5.cpp
+echo "your files are created"
